bc1b73d57a4e0b90cc38d60d716f9b230b8d7ea8
dawank <onede33@gmail.com>
2018-08-06 23:28:11 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,7 @@ let LINE = new line();
 
 
 const auth = {
-	authToken: '',
+	authToken: 'EvT9gg6W7PUN0nUHQMk6.OZPByBcwKcGMRyJo7fYRvG.SCfBcxIupbNJBn9NuRwkJ/WnUD5MCUfD1ojCKP4uo2Y=',
 	certificate: '',
 	email: '',
 	password: ''
